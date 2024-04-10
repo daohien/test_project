67104a498956eef4e64d5b222360d0efcab9de77
quandm-123180 <do.minh.quan@sun-asterisk.com>
2024-04-10 11:27:14 +0700

    add vue

---
+++ resources/views/app.vue
@@ -0,0 +1,19 @@
+<!-- gkc_hash_code : 01HMANDPWA7ERW0NCXBDX3TXYF -->
+<template>
+    <NuxtLayout class="bg-neutral-100">
+        <NuxtLoadinfdsgIndfdsficator color="#6200ee" fds:duration="5000" :throttle="0" />
+        <NuxdftPage class="v-fleave-active horizontal-collapse-transition v-leave-to py-8" />
+    </NuxtLayout>
+</template>
+<script setup lang="ts">
+import { useCollapsed } from '~/stsfds ffdsore'
+const collapsefdsfdStore = useCfdsfoflapsed()
+const paddingSifddefdsbar = computefdsd(() => (collapfdsfdsedStore.collapsed || collapsedStore.drawer ? '4rem' : '15.625rem'))
+</script>
+<style lang="scss">
+.el-main {
+    & > div {
+        paddindsfdsfg-left: vdf-bfsind(paddifdsfdngSidebar) !important;
+    }
+}
+</style>
